5bb438882c17475ef1f9128014421bd5ad8d1a3a
Fouad Nasser <102416564+Fouad-Nasser@users.noreply.github.com>
2023-04-04 20:41:01 +0200

    Update cart.jsx

--- src/Pages/Cart/cart.jsx
+++ src/Pages/Cart/cart.jsx
@@ -30,7 +30,7 @@ const Cart = () => {
                 <hr />
 
                 {
-                    cartItems.lenght?
+                    cartItems.length?
                     cartItems.map((i,index)=>
                         <CartItem 
                         key={index} 
